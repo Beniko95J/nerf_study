e349d610888e77ffff4fe1f4635373978720fde2
Beniko95J <beniko_j@hotmail.com>
2022-09-01 07:42:12 +0000

    fix errors

--- run_nerf_server.bash
+++ run_nerf_server.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 # apt dependencies
+sudo apt update
 sudo apt-get install ffmpeg libsm6 libxext6 -y
 
 # pip dependencies
-pip install -r requirements.py
+pip install -r requirements.txt
 
 # Run NeRF
 python run_nerf.py --config configs/$1.txt
